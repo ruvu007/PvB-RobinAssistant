b2f9360e5d4f22c61c53d5cfee1ab5dd58e9253e
Ruben V <ralvuurman@gmail.com>
2022-03-14 21:56:18 +0100

    Small fixes

--- Code/robin-assists/src/components/ConvinceYouLogin.vue
+++ Code/robin-assists/src/components/ConvinceYouLogin.vue
@@ -1,8 +1,8 @@
 <template>
-    <div class="convince-you-login">
+    <div class="convince-you-login" :style="{'background-image':'url(https://cdn.discordapp.com/attachments/618177603202842667/952999276533936138/Mask_Group_5.png)'}">
         <div class="cyl-tekst">
-            <h2>Hebben we je al overtuigd? Koop Robin Assistant vandaag en begin anderen te helpen.</h2>
-            <a href="https://robinassists.me/dashboard/">Login</a>
+            <h2 class="overtuigd-title">Hebben we je al overtuigd? Download Robin Assistant vandaag en begin anderen te helpen.</h2>
+            <a class="login-button" href="https://robinassists.me/dashboard/">Login</a>
         </div>
     </div>
 </template>
